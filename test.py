c46f42c98a46411083c3c3abf87638c295a3e7df
Alister19 <alister19adbob@yandex.ru>
2023-09-04 11:43:28 +0300

    first commit

---
+++ test.py
@@ -0,0 +1,4 @@
+print("hello world!")
+x = 5
+y = 220000464
+print(x + y)
---
+++ testing_bin.py
@@ -0,0 +1,32 @@
+input = "arhiv.bin"                                                             # Input File
+output = "output.csv"                                                           # Output File:
+
+delim = 1                                                                       # Insert separator every n bytes
+newline = 10                                                                    # Insert newline every n delimiters
+
+# -------------------
+
+delim = delim * 2                                                               # Bytes in hex are 2 characters long
+
+f=open(input,"rb")
+o=open(output,"w")
+
+content=f.read().hex()                                                          # Read file as hex bytes
+content=','.join(content[i:i+delim] for i in range(0, len(content), delim))     # Insert Delimiters
+content=content.split(',')                                                      # Split into array
+
+cLength = len(content)
+
+for x in range(cLength):                                                        # Convert to Decimal
+  content[x] = int(content[x],16)
+
+for x in range(cLength):                                                        # Insert NewLines
+  if not (x)%newline and x>0:
+    content[x] = "\n" + str(content[x])
+
+content = listToStr = ','.join(map(str, content))                               # Convert to string
+content = content.replace(",\n", "\n")                                          # Remove Trailing Commas
+
+o.write(content)
+o.close()
+f.close()
